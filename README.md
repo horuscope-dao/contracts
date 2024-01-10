1148449fef3ace73e2b028ac944661fad2e2341b
Augustin Berisa <augustin.be@pm.me>
2024-01-10 11:23:54 +0100

    Initialize the project

---
+++ README.md
@@ -0,0 +1,13 @@
+# Horuscope-dao Hardhat
+
+This hardhat project initiates horuscope-dao-contracts development.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat run scripts/deploy.ts
+```
---
+++ scripts/deploy.ts
@@ -0,0 +1,14 @@
+import { ethers, upgrades, run } from 'hardhat'
+import { log } from 'console'
+
+const main = async () => {
+
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
